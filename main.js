7de1ad71e4f9df470452266966bbc0162e9704ab
Ivan Vedernikov <ivedernikov@me.com>
2019-06-29 16:00:24 +0300

    fix main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var Track = function (params) {
+  this.name = params.name;
+  this.url = params.url;
+};
+
+var track01 = new Track({
+  name: 'track01',
+  url: 'track01.mp3'
+});
+
+var track02 = new Track({
+  name: 'track02',
+  url: 'track02.mp3'
+});
+
+console.log(track01);
+console.log(track02);
